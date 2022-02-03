7310548fae63f73a7dc858a29ab7662eeb4b5691
mobile44 <albertwong.wk@gmail.com>
2022-02-02 22:09:53 -0800

    enhance: delta page with radio buttons

--- src/Delta.js
+++ src/Delta.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import Linechart from "./hooks/Linechart";
+import { lineChartData } from "./data/Datad3";
+import "./Delta.css";
 
 function Delta() {
   const [selectedGroup, setSelectedGroup] = useState("All");
   const [groupColour, setGroupColour] = useState("lightgrey");
+  const lineGroupunique = lineChartData.map((obj)=>{return obj.group}).filter((item, index,arr)=>{return arr.indexOf(item)===index});
   /*
   function updateBarChart(group, colour) {
     setSelectedGroup(group);
@@ -12,14 +15,23 @@ function Delta() {
   */
   return (
     <div className="deltaPage">
-      <svg viewBox="-2 0 100 100" preserveAspectRatio="xMidYMid meet">
-        <Linechart
-          positionX={10}
-          positionY={10}
-          selectedGroup={selectedGroup}
-          lineColour={groupColour}
-        />
-      </svg>
+      <div className="deltaLine">
+        {lineGroupunique && lineGroupunique.map((k,i)=>(
+          <label className="deltaRadio" key={i}>{k}
+            <input key={i} type="radio" value={i} checked={selectedGroup === k} onChange={()=>setSelectedGroup(k)} />
+            <span className="deltaLabel"></span>
+          </label>
+        ))}
+        <svg viewBox="-2 0 100 100" preserveAspectRatio="xMidYMid meet">
+          <Linechart
+            positionX={10}
+            positionY={10}
+            selectedGroup={selectedGroup}
+            lineColour={groupColour}
+          />
+        </svg>
+      </div>
+      
     </div>
 
   )
