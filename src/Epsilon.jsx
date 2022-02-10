ca913337da01a2d9e2c4c4022158ca142e51a865
mobile44 <albertwong.wk@gmail.com>
2022-02-09 21:47:37 -0800

    add: gsat pin in epsilon page

--- src/Epsilon.jsx
+++ src/Epsilon.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { gsap } from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 import "./Epsilon.css";
 import bgvideo from "./images/videoplayback.mp4"
 
 function Epsilon() {
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(()=>{
+    ScrollTrigger.create({
+      trigger: "#b",
+      start: "top center", //element viewport
+      end: "center 350px", //element viewport
+      pin: "#bb",
+      //markers: true,
+    });
+    ScrollTrigger.create({
+      trigger: "#c",
+      start: "top center", //element viewport
+      end: "center 350px", //element viewport
+      pin: "#cc",
+      //markers: true,
+    });
+    ScrollTrigger.create({
+      trigger: "#d",
+      start: "top center", //element viewport
+      end: "center 350px", //element viewport
+      pin: "#dd",
+      //markers: true,
+    });
+  })
   return (
     <div className="epsilonPage">
+      <div className="epsilonGreen">
       <div className="epsilonContainer">
         <video
           src={bgvideo}
@@ -21,14 +48,12 @@ function Epsilon() {
           <p>What a beautiful world</p>
         </div>
       </div>
-      
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
-      <div><p>heloo</p></div>
+      </div>
+      <section id="a" className="epsilonSection epsilonOrange"><p id="aa">First Section - Pin is next</p></section>
+      <section id="b" className="epsilonSection epsilonPink"><p id="bb"> Second Section - Pin now</p></section>
+      <section id="c" className="epsilonSection epsilonPurple"><p id="cc">Third Section - Pin now</p></section>
+      <section id="d" className="epsilonSection epsilonRed"><p id="dd">Fourth Section - Pin now</p></section>
+      <section id="e" className="epsilonSection epsilonGrey"><p id="ee">Fifth Section - Pin is over</p></section>
     </div>
   )
 }
